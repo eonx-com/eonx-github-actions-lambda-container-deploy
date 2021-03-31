8048a49fe621477de02733930417c0193ce99a6a
Damian Sloane <damian@capacitorlabs.com.au>
2021-03-31 15:41:33 +1100

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,5 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
 cd ${SOURCE_PATH};
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com;
 docker build -t ${REPOSITORY_NAME} .;
-docker tag ${REPOSITORY_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION.amazonaws.com/${REPOSITORY_NAME}:${GITHUB_SHA};
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION.amazonaws.com/${REPOSITORY_NAME}:${GITHUB_SHA};
+docker tag ${REPOSITORY_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${REPOSITORY_NAME}:${GITHUB_SHA};
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${REPOSITORY_NAME}:${GITHUB_SHA};
